a255e636a6a4eca6978f68f06179d565d6b1be98
Jonathas <johnnyxpjourney@gmail.com>
2022-09-26 17:52:48 -0300

    Adiciona o arquivo 03-variaveis_constantes

---
+++ 03-variaveis_constantes.py
@@ -0,0 +1,14 @@
+nome = "Jonathas"
+idade = 16
+
+nome, idade = "Sabrina", 15
+
+print(nome, idade)
+
+# Nomes sugestivos
+limite_saque_diario = 1000
+
+# Constantes
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS", "MG"]
+
+print(BRAZILIAN_STATES)
