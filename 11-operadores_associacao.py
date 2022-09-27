99e5b849f5d89abeb7ef46fa3bb3c7ddb38cc735
Jonathas <johnnyxpjourney@gmail.com>
2022-09-27 16:16:06 -0300

    Adiciona o arquivo 11-operadores_associacao

---
+++ 11-operadores_associacao.py
@@ -0,0 +1,9 @@
+frutas = ["Limão", "Uva"]
+curso = "Curso de Python"
+
+print("Laranja" in frutas)
+print("Limão" not in frutas)
+print("Laranja" not in frutas)
+print("Limão" in frutas)
+
+print("Python" in curso)
