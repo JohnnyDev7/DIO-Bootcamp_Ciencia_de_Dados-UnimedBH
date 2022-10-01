e599b4e42217c0cffdd5c5dfc50f7414f1a7ded6
Jonathas <johnnyxpjourney@gmail.com>
2022-10-01 13:59:39 -0300

    Adiciona o arquivo 22-string_4

---
+++ 22-string_4.py
@@ -0,0 +1,23 @@
+nome = "Jonathas"
+
+mensagem = f"""
+    Olá, meu nome é {nome}
+Estou aprendendo Python.
+        Essa mensagem tem diferentes recuos.
+"""
+
+print(mensagem)
+
+print(
+    '''
+    ============= MENU =============
+
+    1 - Depositar
+    2 - Sacar
+    0 - Sair
+
+    ================================
+
+Obrigado por utilizar nosso sistema!!!
+'''
+)
